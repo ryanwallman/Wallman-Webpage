c69f9f1b038146d7f83c158d3c785b75c24f12cb
Ryan Wallman <ryanwallman7@gmail.com>
2025-03-27 17:28:20 -0400

    added timeline

--- script.js
+++ script.js
@@ -303,4 +303,30 @@ if (window.innerWidth <= 600) {
       // For now, we don't need to do anything specific here
     });
   }
-  
+
+
+  window.addEventListener("load", function () {
+    const skillIcons = document.querySelectorAll(".skills-grid img");
+    const timelineItems = document.querySelectorAll(".timeline-item");
+
+    function createObserver(elements, delay = 150) {
+        const observer = new IntersectionObserver((entries, observer) => {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting) {
+                    let elementIndex = [...elements].indexOf(entry.target);
+                    
+                    setTimeout(() => {
+                        entry.target.classList.add("show");
+                    }, elementIndex * delay);
+
+                    observer.unobserve(entry.target);
+                }
+            });
+        }, { threshold: 0.1 });
+
+        elements.forEach(el => observer.observe(el));
+    }
+
+    createObserver(skillIcons, 100);
+    createObserver(timelineItems, 300);
+});
